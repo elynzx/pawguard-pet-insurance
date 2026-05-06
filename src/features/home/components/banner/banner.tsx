90421876ada3ee6747700ac580a8bf404c3d0886
Elynzx <rpascual06@outlook.com>
2026-05-06 10:39:42 -0500

    fix: update text and icon colors for improved visual consistency

--- src/features/home/components/banner/banner.tsx
+++ src/features/home/components/banner/banner.tsx
@@ -8,7 +8,7 @@ export const Banner = () => {
     <section className="w-full overflow-hidden relative">
       <div className="flex w-full md:max-w-432 px-4 md:px-30 gap-20 items-center">
         <div className="flex-1 flex flex-col items-center jusify-start relative z-10 gap-2">
-          <BoneIcon size={80} className="absolute top-3 text-primary/80"/>
+          <BoneIcon size={80} className="absolute top-3 text-secondary/80"/>
           <CurvedText />
           <h2 className="text-5xl font-black mt-8 mb-8 font-heading text-center">
             Mascotas felices, dueños tranquilos.
