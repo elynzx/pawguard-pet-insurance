74e72d3ef652b4401665a6c11f5c8f314a668aca
Elynzx <rpascual06@outlook.com>
2026-05-11 11:28:32 -0500

    feat: add routing for Login and Profile with ProtectedRoute

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,6 +3,9 @@ import { Home } from "../app/home/home";
 import { Checkout } from "../app/checkout/checkout";
 import { MainLayout } from "../common/layouts/main-layout";
 import { CheckoutLayout } from "../common/layouts/checkout-layout";
+import { Login } from "../app/login/login";
+import { Profile } from "../app/profile/profile";
+import { ProtectedRoute } from "../features/auth/components/protected-route";
 
 export const router = createBrowserRouter([
   {
@@ -12,18 +15,30 @@ export const router = createBrowserRouter([
       {
         index: true,
         Component: Home,
+      },
+      {
+        element: <ProtectedRoute />, 
+        children: [
+          {
+            path: "profile",
+            Component: Profile,
+          }
+        ]
       }
     ]
+  },
+  {
+    path: "/login",
+    Component: Login,
   },
   {
     path: "/checkout",
     Component: CheckoutLayout,
     children: [
       {
-        index:true,
+        index: true,
         Component: Checkout,
       }
     ]
-
   }
-])
+]);
